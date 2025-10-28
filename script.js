06e8092b1b0515f4913883f0a30553e5a0d6687d
“cristopher” <curibe26@aoiths.org>
2025-10-28 13:44:32 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // ===== FUNCTIONS WITH NO PARAMETERS ===== //
-
+function sayhello() {
+    let output1 = document.querySelector("#output1") 
+    output1.innerHTML = "Hello roger! welcome to JavaScript functions!"
+}
 
 
 
